c8593c6de5f7782c3fe42f780196ef279dd1082c
AitorTrevino <aitor.trebinot@gmail.com>
2025-02-25 11:13:25 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+#Ejercicio email cadena
+cadena_email = "Hola, buenos días."
+cadena_email = cadena_email.replace('Hola', 'adiós')
+print(cadena_email)
+
+#Ejercicios M2C3 Python Assignment
+
+#Ejercicio 1
+string = "Hello there"
+number = 30
+list = ["element1", "element2", "element3"]
+boolean = True
+
+#Ejercicio 2
+indexed_string = string[0:3]
+print(indexed_string)
+
+#Ejercicio 3
+
+indexed_list = list[0]
+print(indexed_list)
+
+#Ejercicio 4
+
+num_plus_ten = number + 10
+print(num_plus_ten)
+
+#Ejercicio 5
+
+last_of_list = list[-1]
+print(last_of_list)
+
+#Ejercicio 6
+
+names = 'harry,alex,susie,jared,gail,conner'
+names_list = names.split(',')
+print(names_list)
+
+#Ejercicio 7
+string_firstword = string[0:5]
+uppercase_word = string_firstword.upper()
+new_string = uppercase_word + string[5:]
+print(new_string)
+
+#Ejercicio 8
+ex8_sentence = f"This {number} is a number" 
+print(ex8_sentence)
+
+#Ejercicio 9
+print("hello world")
